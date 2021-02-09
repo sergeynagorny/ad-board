bd2eb65b9c68dd533e81b6f23628ffb90b3999e3
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-02-09 15:12:47 +0300

    🐣

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+let mySlider = new rSlider({
+  target: `#sampleSlider`,
+  values: [10000, 1000000],
+  range: true,
+  tooltip: true,
+  scale: true,
+  labels: false,
+  step: 10000
+});
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    open: true,
+    watchContentBase: true,
+    port: 1337,
+  },
+};
