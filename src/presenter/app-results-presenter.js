1344ba20e296670389b744d309ff9c2e64311c53
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-02-15 16:08:19 +0300

    Обновил `AppResultsPresenter`
    - Прокинул `ProductsModel` и `FavoriteModel`
    - Описал ивенты моделей
    - Описал логику функции получения товаров
    - Описал логику отрисовки пустых экранов
    - Обновил логику рендеринга и очистки товаров

--- src/presenter/app-results-presenter.js
+++ src/presenter/app-results-presenter.js
@@ -1,4 +1,4 @@
-import {sortingButtons, SortType} from "../const";
+import {sortingButtons, SortType, UpdateType} from "../const";
 import {render} from "../utils/render";
 import {getSortedProducts} from "../utils/sort";
 
@@ -9,11 +9,13 @@ import EmptyProductsMessageView from "../view/empty-products-message-view";
 import EmptyFavoritesMessageView from "../view/empty-favorites-message-view";
 import ProductCardsPresenter from "./product-cards-presenter";
 
-
 export default class AppResultsPresenter {
-  constructor(appContainer, products) {
+  constructor(appContainer, productsModel, favoritesModel) {
     this._appContainer = appContainer;
-    this._products = products;
+    this._resultsListContainer = null;
+
+    this._productsModel = productsModel;
+    this._favoritesModel = favoritesModel;
 
     this._appResultsView = null;
     this._sortingOrderView = null;
@@ -22,16 +24,23 @@ export default class AppResultsPresenter {
     this._emptyFavoritesMessageView = null;
     this._productCardsPresenter = null;
 
-    this._showedProductCardsPresenters = [];
+    this._productCardsPresenters = [];
 
     this._selectedSortType = SortType.POPULAR;
     this._sortingButtons = sortingButtons;
 
-    this._handleSortingOrderChange = this._handleSortingOrderChange.bind(this);
+    this.handleSortingOrderChange = this.handleSortingOrderChange.bind(this);
+    this.handleFavoritesShown = this.handleFavoritesShown.bind(this);
+
+    this.handleFavoritesModelEvent = this.handleFavoritesModelEvent.bind(this);
+    this.handleProductsModelEvent = this.handleProductsModelEvent.bind(this);
+    this._favoritesModel.addSubscriber(this.handleFavoritesModelEvent);
+    this._productsModel.addSubscriber(this.handleProductsModelEvent);
   }
 
   render() {
     this._appResultsView = new AppResultsView();
+    this._resultsListContainer = this._appResultsView.getResultsListContainer();
 
     this._renderSorting();
     this._renderResultsList();
@@ -42,9 +51,34 @@ export default class AppResultsPresenter {
 
   // HANDLERS
 
-  _handleSortingOrderChange(sortType) {
+  handleSortingOrderChange(sortType) {
     this._selectedSortType = sortType;
     this._renderResultsList();
+  }
+
+  handleFavoritesShown(isShown) {
+    this._favoritesModel.setIsFavoritesShown(isShown);
+  }
+
+
+  // EVENTS
+
+  handleFavoritesModelEvent() {
+    const isSortingOrderDisabled = this._favoritesModel.getIsFavoritesShown();
+    this._sortingOrderView.disabled(isSortingOrderDisabled);
+    this._renderResultsList();
+  }
+
+  handleProductsModelEvent(updateType, newProduct) {
+    switch (updateType) {
+      case UpdateType.MINOR:
+        const productCardsPresenters = this._productCardsPresenters.find((presenter) => presenter.id === newProduct.id);
+        productCardsPresenters.render(newProduct);
+        break;
+
+      default:
+        this._renderResultsList();
+    }
   }
 
 
@@ -53,21 +87,30 @@ export default class AppResultsPresenter {
   _renderSorting() {
     const sortingContainer = this._appResultsView.getSortingContainer();
 
-    const sortingOrderView = new SortingOrderView(this._sortingButtons, this._selectedSortType);
-    const sortingFavoritesView = new SortingFavoritesView();
-    sortingOrderView.setSortingOrderChangeHandler(this._handleSortingOrderChange);
+    this._sortingOrderView = new SortingOrderView(this._sortingButtons, this._selectedSortType);
+    this._sortingFavoritesView = new SortingFavoritesView();
+    this._sortingOrderView.setSortingOrderChangeHandler(this.handleSortingOrderChange);
+    this._sortingFavoritesView.setFavoritesShownHandler(this.handleFavoritesShown);
 
-    render(sortingContainer, sortingOrderView);
-    render(sortingContainer, sortingFavoritesView);
+    render(sortingContainer, this._sortingOrderView);
+    render(sortingContainer, this._sortingFavoritesView);
   }
 
   _getProducts() {
-    return getSortedProducts(this._products, this._selectedSortType);
+    let products = this._productsModel.getProducts();
+    const isFavoritesShown = this._favoritesModel.getIsFavoritesShown();
+
+    if (isFavoritesShown) {
+      // TODO: Вынести фильтрацию в отдельную функцию
+      products = products.slice().filter((product) => product.isFavorite === true);
+    }
+
+    return getSortedProducts(products, this._selectedSortType);
   }
 
   _renderProductCards(container, products) {
     return products.map((product) => {
-      const productCardsPresenter = new ProductCardsPresenter(container, this._appContainer.parentNode);
+      const productCardsPresenter = new ProductCardsPresenter(container, this._appContainer.parentNode, this._productsModel);
       productCardsPresenter.render(product);
 
       return productCardsPresenter;
@@ -75,15 +118,33 @@ export default class AppResultsPresenter {
   }
 
   _removeProductCards() {
-    this._showedProductCardsPresenters.forEach((productCardsPresenter) => productCardsPresenter.destroy());
-    this._showedProductCardsPresenters = [];
+    this._productCardsPresenters.forEach((productCardsPresenter) => productCardsPresenter.destroy());
+    this._productCardsPresenters = [];
+  }
+
+  _renderEmptyScreens() {
+    const isFavoritesShown = this._favoritesModel.getIsFavoritesShown();
+
+    if (isFavoritesShown) {
+      render(this._resultsListContainer, new EmptyFavoritesMessageView());
+    } else {
+      render(this._resultsListContainer, new EmptyProductsMessageView());
+    }
+  }
+
+  _clearResultsList() {
+    this._resultsListContainer.innerHTML = ``;
+    this._removeProductCards();
   }
 
   _renderResultsList() {
-    this._removeProductCards();
+    this._clearResultsList();
     const products = this._getProducts();
 
-    const resultsListContainer = this._appResultsView.getResultsListContainer();
-    this._showedProductCardsPresenters = this._renderProductCards(resultsListContainer, products);
+    if (products.length === 0) {
+      this._renderEmptyScreens();
+    }
+
+    this._productCardsPresenters = this._renderProductCards(this._resultsListContainer, products);
   }
 }
