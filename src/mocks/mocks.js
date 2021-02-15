29be30ce1b22e0e12f8ca02c3650c69cb87e3be4
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-02-15 15:46:19 +0300

    Обновил генерацию моковых данных
    Добавил:
    - Свойство `id`
    - Свойство `isFavorite`
    - Обновил генерацию фотографий

--- src/mocks/mocks.js
+++ src/mocks/mocks.js
@@ -13,6 +13,14 @@ const getRandomPrice = () => {
   return Number(`${getRandomInteger(0, 100)}000`);
 };
 
+const photos = [`img/item1.jpg`, `img/item2.jpg`, `img/item3.jpg`, `img/item4.jpg`, `img/item5.jpg`, `img/item6.jpg`];
+
+const shuffleArray = (arr) => {
+  return arr.map((a) => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map((a) => a[1]);
+};
+
 
 const filterByCategory = {
   "Автомобиль": function () {
@@ -49,12 +57,14 @@ const filterByCategory = {
   },
 };
 
-const generateMockItem = () => {
+const generateMockItem = (index) => {
+  const id = index + 1 || null;
   const category = getRandomItem([`Недвижимость`, `Ноутбук`, `Фотоаппарат`, `Автомобиль`]);
   const dateNow = new Date(Date.now());
   const publishDate = new Date(dateNow.setHours(dateNow.getHours() - getRandomInteger(2, 500)));
 
   return ({
+    id,
     name: getRandomItem([`BMW 5 серии б/у`, `LADA (ВАЗ) 2121 (4x4)`, `MacBook Pro 13 2011 года`, `Ноутбук Asus GTX 860`, `Ультрабук Dell XPS 13`, `Фотоаппарат Sony NEX-5`]),
     category: adaptCategory(category),
     price: getRandomPrice(),
@@ -67,19 +77,20 @@ const generateMockItem = () => {
       30.323126,
     ],
     publishDate,
+    isFavorite: Boolean(getRandomInteger(0, 1)),
     description: `Просторная двушка в центре Питера. Без кухни, зато в историческом здании. Внизу много кафешек, поесть можно и там. Есть выход на крышу, с которой виден весь город. В собственности больше трёх лет.`,
     address: {
       city: getRandomItem([`Хабаровск`, `Гродно`, `Москва`, `Минск`, `Питер`, `Варшава`]),
       street: getRandomItem([`ул. Павловича`, `ул. Поповича`, `проспект Победы`, `ул. Пархоменко`, `ул. Рябиновая`]),
       building: getRandomItem([`д. 19`, `д. 20`, `д. 19 корпус 2`, `64/25`]),
     },
-    photos: new Array(getRandomInteger(0, 7)).fill(`https://placeimg.com/520/340/arch`),
+    photos: shuffleArray(photos),
     filters: filterByCategory[category](),
   });
 };
 
 export const getMockData = (amount) => {
-  return new Array(amount).fill({}).map(() => generateMockItem());
+  return new Array(amount).fill({}).map((_, i) => generateMockItem(i));
 };
 
 
