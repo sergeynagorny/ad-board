33cdc14792b1c6ee20d7eb353873016d758ef5ef
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-02-19 21:44:32 +0300

    Хотфикс стилей для `EmptyMessageView`

--- src/view/empty-favorites-message-view.js
+++ src/view/empty-favorites-message-view.js
@@ -2,7 +2,7 @@ import AbstractView from "./abstract-view";
 
 const createEmptyFavoritesMessageTemplate = () => {
   return /* html */`
-    <div class="results__info results__info--empty-block">
+    <div class="results__info favourites results__info--empty-block">
       <p class="favorites__empty-message">У вас пока нет избранных товаров. Чтобы отметить товар, кликните на сердечко в карточке объявления.</p>
       <p class="favorites__notion">Вы можете вернуться к списку всех товаров, кликнув ещё раз на «Показать избранные»</p>
     </div>
--- src/view/empty-products-message-view.js
+++ src/view/empty-products-message-view.js
@@ -2,7 +2,7 @@ import AbstractView from "./abstract-view";
 
 const createEmptyProductsMessageTemplate = () => {
   return /* html */`
-    <div class="results__info results__info--empty-block">
+    <div class="results__info favourites results__info--empty-block">
       <p class="results__empty-message">Мы не нашли товары по вашему запросу.</p>
       <p class="results__notion">Попробуйте поменять фильтры настройки объявлений в блоке слева</p>
     </div>
